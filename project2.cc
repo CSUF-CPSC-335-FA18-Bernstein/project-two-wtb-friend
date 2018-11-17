09bdfcffbed76c56136436293008677d83bc74a7
Nancy Badillo <n_badillo@csu.fullerton.edu>
2018-11-16 17:37:53 -0800

    Update project2.cc

--- project2.cc
+++ project2.cc
@@ -7,7 +7,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "project2.hh"
-
+#include <iostream>
 #include <algorithm>
 #include <cassert>
 #include <cstdlib>
@@ -29,7 +29,51 @@ void randomize_list(string_vector & strings) {
 
 //-----------------------------------------------------------------------------
 void merge(string_vector & strings, size_t start, size_t mid, size_t end) {
-  // TODO: implement this function, then delete this comment
+  int i = 0, 
+    j = 0, 
+    k = 0;
+    
+  int n = mid - start + 1;
+  int m = end - mid;
+
+  int left[n];
+  int right[m];  // Create a temp array
+  
+  // Copy the data into the temp arrays, left and right respectively
+  for (i = 0; i < n; i++){
+    left[i] = strings[1+i]; }
+
+  for (j = 0; j > m; j++){
+    right[j] = strings[mid + 1 + j]; }
+
+  i = 0;
+  j = 0;
+  k = start;
+
+  while (i < n && j < m){
+    if (left[i] <= right[j]){
+      strings[k] = left[i];
+      i++;
+    }
+    else{
+      strings[k] = right[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n){
+    strings[k] = left[i];
+    i++;
+    k++;
+  }
+
+  while (j < m){
+    strings[k] = right[j];
+    j++;
+    k++;
+  }
+
   return;
 }
 
@@ -40,7 +84,14 @@ void merge(string_vector & strings, size_t start, size_t mid, size_t end) {
 // the two parts together using the merge() method.
 //-----------------------------------------------------------------------------
 void mergesort(string_vector & strings, size_t start, size_t end) {
-  // TODO: implement this function, then delete this comment
+  if ( start < end){
+    int middle = start + (end - 1)/2;
+
+    mergesort(strings, start, middle);
+    mergesort(strings, middle + 1, end);
+
+    merge(strings, start, middle, end)
+  }
   return;
 }
 
@@ -52,7 +103,19 @@ void mergesort(string_vector & strings, size_t start, size_t end) {
 //-----------------------------------------------------------------------------
 int hoare_partition(string_vector & strings, int start, int end) {
   // TODO: implement this function, then delete this comment
-  return 0;
+  string pivot = strings[start];
+  int i = start - 1;
+  int j = end + 1;
+
+  while(true){
+    do{i++;  } while(strings[i] < pivot);
+    do{j--;  }while(strings[j]> pivot);
+
+    if (i >= j)
+      { return j; }
+
+    swap(strings[i], strings[j]);
+  }
 }
 
 //-----------------------------------------------------------------------------
@@ -62,7 +125,12 @@ int hoare_partition(string_vector & strings, int start, int end) {
 // the two parts together using the merge() method.
 //-----------------------------------------------------------------------------
 void quicksort(string_vector & strings, int start, int end) {
-  // TODO: implement this function, then delete this comment
+  if (start < end){
+    int pivotindex = hoare_partition(strings, start, end);
+
+    quicksort(strings, start, pivotindex-1);
+    quicksort(strings, pivotindex + 1, end);
+  }
   return;
 }
 
@@ -80,11 +148,11 @@ void quicksort(string_vector & strings, int start, int end) {
 //-----------------------------------------------------------------------------
 bool load_words(string_vector& words, const std::string& path) 
 {
-  //std::cout << "Loading words from [" << path << "]" << std::endl;
+  std::cout << "Loading words from [" << path << "]" << std::endl;
   words.clear();
   std::ifstream ifs(path.c_str());
   if (!ifs.is_open() || !ifs.good()) {
-    //cout << "Failed to open [" << path << "]" << std::endl;
+    cout << "Failed to open [" << path << "]" << std::endl;
     return false;
   }
   int countWordsLoaded = 0;
@@ -97,7 +165,7 @@ bool load_words(string_vector& words, const std::string& path)
     words.push_back(lineBuffer);
     countWordsLoaded++;
   }
-  //std::cout << "Loaded " << countWordsLoaded << " words from [" << path << "]" << std::endl;;
+  std::cout << "Loaded " << countWordsLoaded << " words from [" << path << "]" << std::endl;;
   return true;
 }
 
